3b05a8f6998b8795a01cc640ef925d69860b954d
Aaron Martin <aaron.martin@shopgate.com>
2018-11-20 11:24:37 -0600

    CCP-955: added changes based on feedback of pull request review

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,26 +1,16 @@
-import { themeConfig } from '@shopgate/pwa-common/helpers/config';
 import getConfig from './helpers/getConfig';
 
-const { background, primary } = themeConfig.colors;
-
 const {
-  headerBackgroundColor,
-  headerColor,
+  headerHomePageBackgroundColor,
+  headerHomeTextColor,
   headerBackgroundColorDefault,
-  headerColorDefault,
+  headerTextColorDefault,
 } = getConfig();
 
-export const HEADER_BACKGROUND_COLOR =
-               (headerBackgroundColor !== undefined && headerBackgroundColor !== null)
-                 ? headerBackgroundColor : background;
-export const HEADER_COLOR = (headerColor !== undefined && headerColor !== null)
-  ? headerColor : primary;
-export const HEADER_BACKGROUND_COLOR_DEFAULT =
-               (headerBackgroundColorDefault !== undefined && headerBackgroundColorDefault !== null)
-                 ? headerBackgroundColorDefault : background;
-export const HEADER_COLOR_DEFAULT =
-               (headerColorDefault !== undefined && headerColorDefault !== null)
-                 ? headerColorDefault : primary;
+export const HEADER_BACKGROUND_COLOR = headerHomePageBackgroundColor || null;
+export const HEADER_COLOR = headerHomeTextColor || null;
+export const HEADER_BACKGROUND_COLOR_DEFAULT = headerBackgroundColorDefault || null;
+export const HEADER_COLOR_DEFAULT = headerTextColorDefault || null;
 export const SET_NAVIGATOR_BACKGROUND = 'SET_NAVIGATOR_BACKGROUND';
 export const SET_NAVIGATOR_COLOR = 'SET_NAVIGATOR_COLOR';
 
