f7251cec02f1ff26a58493d0a5d38b1887990e0a
Aaron Martin <aaron.martin@shopgate.com>
2018-11-21 08:48:48 -0600

    CCP-955: improved configuration name

--- frontend/constants.js
+++ frontend/constants.js
@@ -2,13 +2,13 @@ import getConfig from './helpers/getConfig';
 
 const {
   headerHomePageBackgroundColor,
-  headerHomeTextColor,
+  headerHomePageTextColor,
   headerBackgroundColorDefault,
   headerTextColorDefault,
 } = getConfig();
 
 export const HEADER_BACKGROUND_COLOR = headerHomePageBackgroundColor || null;
-export const HEADER_COLOR = headerHomeTextColor || null;
+export const HEADER_COLOR = headerHomePageTextColor || null;
 export const HEADER_BACKGROUND_COLOR_DEFAULT = headerBackgroundColorDefault || null;
 export const HEADER_COLOR_DEFAULT = headerTextColorDefault || null;
 export const SET_NAVIGATOR_BACKGROUND = 'SET_NAVIGATOR_BACKGROUND';
