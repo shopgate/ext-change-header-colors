97827ffa3788995ff4c534f1918041e8dddff141
Carsten Schneider <carsten.schneider@shopgate.com>
2019-06-17 11:51:59 +0200

    CCP-1557 removed old configs

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,13 +1,10 @@
 import getConfig from './helpers/getConfig';
 
 const {
-  headerHomePageBackgroundColor,
-  headerHomePageTextColor,
   backgroundColor,
   textColor,
 } = getConfig();
 
-// Use old config values headerHomePageBackgroundColor, headerHomePageTextColor if they still exist
-export const HEADER_BACKGROUND_COLOR = headerHomePageBackgroundColor || backgroundColor || null;
-export const HEADER_COLOR = headerHomePageTextColor || textColor || null;
+export const HEADER_BACKGROUND_COLOR = backgroundColor || null;
+export const HEADER_COLOR = textColor || null;
 
